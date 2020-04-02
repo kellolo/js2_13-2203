542f98306df7fb9fa0c23a6cd0e873206a696cab
Elena Bokhan <clevergirl_777@mail.ru>
2020-04-02 17:24:58 +0400

    makeFakeRequest v3.0

--- Students/Elena Bokhan/project/src/index.js
+++ Students/Elena Bokhan/project/src/index.js
@@ -1,10 +1,6 @@
 import "./public/css/style.css"
 import "./public/css/normalize.css"
-<<<<<<< HEAD
 import "./public/js/main.js"
 
 
       
-=======
-import "./public/js/main.js"
->>>>>>> 5161b9d915b6f39951e6827f7a8724f8a48f550e
