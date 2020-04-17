b946e2ca43773dd45ec0b072c1beb796cf199e78
alexey_skrrynnikov <aleks_skr@mail.ru>
2020-04-17 22:39:55 +0300

    8.1

--- Students/Skrynnikov Alexey/project/src/public/app.vue
+++ Students/Skrynnikov Alexey/project/src/public/app.vue
@@ -3,25 +3,21 @@
     <header>
         <div class="logo">E-shop</div>
         <div class="cart">
-            <form action="#" class="search-form">
-                <input type="text" class="search-field">
-                <button class="btn-search">
-                    <i class="fas fa-search"></i>
-                </button>
-            </form>
+            <searchComponent @search="refs.catalog.filter"/>
             <button class="btn-cart" @click="show=!show">Cart</button>
             <cart ref="cartRef"/>            
         </div>
     </header>
     <main>
-        <catalog />
+        <catalog ref="catalog"/>
     </main>
   </div>
 </template>
 
 <script>
-import cart from './containers/cart.vue'
-import catalog from './containers/catalog.vue'
+import cart from './containers/cart.vue';
+import catalog from './containers/catalog.vue';
+import searchComponent from './components/searchComponent.vue';
 
 export default {
     components: { cart, catalog },
